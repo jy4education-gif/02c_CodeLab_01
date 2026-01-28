f4218d0bf06ea08f9e8f4ae8fc6dde18f9d48b2e
jy4education_git <jy4education@gmail.com>
2026-01-28 08:49:14 +0100

    +

---
+++ com/cc/java/Konto.java
@@ -0,0 +1,19 @@
+package com.cc.java;
+
+public class Konto {
+
+    private int kontostand;    
+
+    public Konto(int kontostand) {
+        this.kontostand = kontostand;
+    }
+
+    public int getKontostand() {
+        return kontostand;
+    }
+
+    public void setKontostand(int kontostand){
+        this.kontostand = kontostand;
+    }
+  
+}
